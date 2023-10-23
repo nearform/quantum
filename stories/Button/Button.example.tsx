99660338469c4fc9295a16a0e58c06231a9e5851
Adam Laker Illoul <85499621+Ademsk1@users.noreply.github.com>
2023-10-23 13:16:34 +0100

    fix: typing of props

--- stories/Button/Button.example.tsx
+++ stories/Button/Button.example.tsx
@@ -1,5 +1,5 @@
-import { Button } from '@/components/Button'
+import { Button, type ButtonProps } from '@/components/Button'
 
-export const TestButton = props => {
+export const TestButton = (props: ButtonProps) => {
   return <Button {...props}>Button text</Button>
 }
