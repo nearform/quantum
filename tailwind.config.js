c2294032e32e1b7eedf711d8c2ddf0fdcbff286a
Adam Laker Illoul <85499621+Ademsk1@users.noreply.github.com>
2023-10-25 17:18:35 +0100

    fix: consistent units 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,8 @@ module.exports = {
       DEFAULT: '0px 1px 2px -1px',
       md: '0px 2px 4px -2px',
       lg: '0px 4px 6px 0px',
-      xl: '0 20px 25px -5px',
-      '2xl': '0 25px 50px -12px',
+      xl: '0px 20px 25px -5px',
+      '2xl': '0px 25px 50px -12px',
       none: '0px 0px'
     },
     extend: {
