1dff9a25d0833094506d06df0f92409990094ac3
Adam Laker Illoul <85499621+Ademsk1@users.noreply.github.com>
2023-10-27 10:52:44 +0100

    fix: remove svg-to-ts, rename files

---
+++ src/assets/build/radioselected.icon.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+const RadioSelected = (props: any) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" {...props}>
+    <circle cx={10} cy={10} r={4} />
+  </svg>
+)
+export default RadioSelected
