da16f9b8b7a0e085f305a972e946a9265d5b8665
Adam Laker Illoul <85499621+Ademsk1@users.noreply.github.com>
2023-10-27 14:00:02 +0100

    fix: centre svg

--- src/assets/build/radioselected.icon.tsx
+++ src/assets/build/radioselected.icon.tsx
@@ -1,6 +1,7 @@
-const RadioSelected = (props: any) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" {...props}>
-    <circle cx={10} cy={10} r={4} />
+export const RadioSelected = (props: any) => (
+  <svg xmlns="http://www.w3.org/2000/svg" {...props} viewBox="0 0 20 20">
+    <g id="State=Selected">
+      <circle id="Ellipse 1" cx={10} cy={10} r={4} />
+    </g>
   </svg>
 )
-export default RadioSelected
