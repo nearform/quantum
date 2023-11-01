8baaedd2829bfa8e133537d2a90f99effd50bea6
Adam Laker Illoul <85499621+Ademsk1@users.noreply.github.com>
2023-11-01 14:57:52 +0000

    fix: use description from figma, update darkmode radio

--- src/components/Radio/RadioGroupItem.tsx
+++ src/components/Radio/RadioGroupItem.tsx
@@ -1,11 +1,30 @@
 import * as React from 'react'
 import * as RadioGroupPrimitive from '@radix-ui/react-radio-group'
 import { cn } from '@/lib/utils'
+import { cva } from 'class-variance-authority'
 import { RadioSelected } from '@/assets'
 
 type RadioGroupItemProps = React.ComponentPropsWithoutRef<
   typeof RadioGroupPrimitive.Item
 >
+
+const radioVariant = cva([
+  [
+    'h-5 w-5',
+    'rounded-full',
+    'border-[0.533px]',
+    'border-border',
+    'bg-grey-50',
+    'disabled:opacity-50',
+    'text-foreground-inverse'
+  ],
+  [
+    'data-[state=checked]:border-primary-600',
+    'data-[state=checked]:bg-primary-600',
+    'data-[state=checked]:bg-primary-600'
+  ],
+  ['dark:bg-grey-700', 'dark:border-grey-500']
+])
 
 const RadioGroupItem = React.forwardRef<
   React.ElementRef<typeof RadioGroupPrimitive.Item>,
@@ -14,10 +33,7 @@ const RadioGroupItem = React.forwardRef<
   return (
     <RadioGroupPrimitive.Item
       ref={ref}
-      className={cn(
-        'h-5 w-5 rounded-full border-[0.67px] border-border bg-grey-50 disabled:opacity-50 data-[state=checked]:border-primary-600 data-[state=checked]:bg-primary-600 text-foreground-inverse',
-        className
-      )}
+      className={cn(radioVariant(), className)}
       {...props}
     >
       <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
