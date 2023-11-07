82abe54bf9548d2ccfac4f856932adc91d21755f
Fergus Ruston <fergus.ruston@nearform.com>
2023-11-07 16:01:46 +0000

    feat: update tsup config to use colors/index.js as an entry point

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/global.css'],
+  entry: ['src/index.ts', 'src/global.css', 'src/colors/index.js'],
   dts: { entry: ['src/index.ts'] },
   clean: true,
   sourcemap: true
