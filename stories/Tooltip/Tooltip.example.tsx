d85271b92c064b8504da410ebb54ca3927350890
Adam Laker Illoul <85499621+Ademsk1@users.noreply.github.com>
2023-11-09 18:01:30 +0000

    feat: add arrow, some colors

--- stories/Tooltip/Tooltip.example.tsx
+++ stories/Tooltip/Tooltip.example.tsx
@@ -2,21 +2,25 @@ import {
   Tooltip,
   TooltipTrigger,
   TooltipContent,
-  TooltipProvider
+  TooltipProvider,
+  TooltipArrow
 } from '@/index'
 import { Button } from '@/index'
 
 export const ToolTipDemo = () => {
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger>
-          <Button>Tooltip test</Button>
-        </TooltipTrigger>
-        <TooltipContent>
-          <p>Add to library</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <div className="h-40 w-40 bg-white flex items-center justify-center">
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <Button>Tooltip test</Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Add to library</p>
+            <TooltipArrow />
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
   )
 }
