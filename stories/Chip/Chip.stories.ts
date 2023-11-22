f84106655f248144ef6e0b41eb12c14e36a49ec1
Adam Laker Illoul <85499621+Ademsk1@users.noreply.github.com>
2023-11-22 17:46:38 +0000

    fix: remove null as a type

--- stories/Chip/Chip.stories.ts
+++ stories/Chip/Chip.stories.ts
@@ -9,6 +9,11 @@ const meta = {
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered'
+  },
+  argTypes: {
+    size: {
+      options: ['default', 'lg']
+    }
   }
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
