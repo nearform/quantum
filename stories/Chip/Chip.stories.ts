4630db1e872f27b7952a23da5209f485f77cdfa7
Adam Laker Illoul <85499621+Ademsk1@users.noreply.github.com>
2023-11-22 17:45:41 +0000

    fix: remove other sizes

--- stories/Chip/Chip.stories.ts
+++ stories/Chip/Chip.stories.ts
@@ -9,14 +9,8 @@ const meta = {
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered'
-  },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  argTypes: {
-    size: {
-      options: ['xs', 'sm', 'md', 'lg', 'xl '],
-      control: 'radio'
-    }
   }
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 } satisfies Meta<typeof Chip>
 
