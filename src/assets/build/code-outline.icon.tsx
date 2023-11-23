9f7ec0680f86be3541fc805928ebbcaf90c1ee3c
Adam Laker Illoul <85499621+Ademsk1@users.noreply.github.com>
2023-11-23 09:41:17 +0000

    feat: add in oss icons

---
+++ src/assets/build/code-outline.icon.tsx
@@ -0,0 +1,19 @@
+const CodeOutline = (props: any) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    fill="none"
+    {...props}
+  >
+    <path
+      stroke="inherit"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="m7 8-4 4 4 4m10-8 4 4-4 4M13 5l-2 14"
+    />
+  </svg>
+)
+export default CodeOutline
