51dde47ad7a2d8039f0a4c00392fe8dba75ffdf3
Adam Laker Illoul <85499621+Ademsk1@users.noreply.github.com>
2023-11-23 13:20:36 +0000

    feat: add angle-down and calendar

--- src/assets/build/angle-down.icon.tsx
+++ src/assets/build/angle-down.icon.tsx
@@ -3,6 +3,7 @@ export const AngleDownIcon = (props: any) => (
     xmlns="http://www.w3.org/2000/svg"
     width={12}
     height={13}
+    viewBox="0 0 12 13"
     fill="none"
     {...props}
   >
--- src/assets/build/calendar.icon.tsx
+++ src/assets/build/calendar.icon.tsx
@@ -3,6 +3,7 @@ export const CalendarIcon = (props: any) => (
     xmlns="http://www.w3.org/2000/svg"
     width={16}
     height={17}
+    viewBox="0 0 16 17"
     fill="none"
     {...props}
   >
