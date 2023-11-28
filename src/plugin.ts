7c1ed45bf6040032ab77ef8d3afd3f6649f824c5
Adam Laker Illoul <85499621+Ademsk1@users.noreply.github.com>
2023-11-28 14:09:17 +0000

    fix: imports as ESM

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,27 +1,28 @@
-const quantumConfig = require('../tailwind.config.js')
-const plugin = require('tailwindcss/plugin')
-const path = require('path')
+import quantumConfig from '../tailwind.config'
+import plugin from 'tailwindcss/plugin'
+import path from 'path'
+import { Config } from 'tailwindcss'
 
-const quantumPlugin = (node_modules_path?: string) => {
-  return plugin(
-    ({ config }: any) => {
-      const twConfig = config()
-      let quantumPkgLoc = './node_modules/@nearform/quantum/dist/index.js'
-      quantumPkgLoc = path.join(
-        node_modules_path ?? '',
-        './node_modules/@nearform/quantum/dist/index.js'
-      )
+const quantumPlugin = plugin(
+  ({ config }: { config: () => Config }) => {
+    const twConfig = config()
+    let quantumPkgLoc = path.join(__dirname, 'index.js')
 
+    if (Array.isArray(twConfig.content)) {
+      if (!twConfig.content.includes(quantumPkgLoc)) {
+        twConfig.content.push(quantumPkgLoc)
+      }
+    } else {
       if (!twConfig.content.files.includes(quantumPkgLoc)) {
         twConfig.content.files.push(quantumPkgLoc)
       }
-    },
-    {
-      theme: {
-        ...quantumConfig.theme
-      }
     }
-  )
-}
+  },
+  {
+    theme: {
+      ...quantumConfig.theme
+    }
+  }
+)
 
 module.exports = quantumPlugin
