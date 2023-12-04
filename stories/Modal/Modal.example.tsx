242890cecebd546fdd3280e1d779fb23f7cdaf02
Yuna Iwata <115478706+yuna-iwata@users.noreply.github.com>
2023-12-04 12:35:02 +0000

    chore: dark mode

--- stories/Modal/Modal.example.tsx
+++ stories/Modal/Modal.example.tsx
@@ -14,25 +14,39 @@ import { Button } from '@/components'
 export const ModalDemo = () => {
   return (
     <Dialog>
-      <DialogTrigger>Open</DialogTrigger>
+      <DialogTrigger className="p-52">
+        <Button size="sm" variant="primary">
+          Open
+        </Button>
+      </DialogTrigger>
       <DialogContent>
-        <DialogHeader className="border-b-[1px] border-border-subtle">
+        <DialogHeader>
           <DialogTitle>Title</DialogTitle>
           <DialogClose />
         </DialogHeader>
-        <DialogDescription>Content</DialogDescription>
-        <DialogFooter className="border-t-[1px] border-border-subtle">
-          <DialogCloseFooter>
-            <Button size="sm" variant="primary">
+        <DialogDescription>
+          <div className="bg-blue-50 h-40 text-lg font-semibold items-center flex justify-center">
+            Replace this component with your content
+          </div>
+        </DialogDescription>
+        <DialogFooter>
+          <DialogCloseFooter className="w-full grid gap-2">
+            <Button
+              size="sm"
+              variant="primary"
+              className="w-full dark:bg-blue-300 dark:border-blue-300 dark:text-foreground"
+            >
               Close
             </Button>
+            <div className="flex justify-center gap-4">
+              <div className="dark:text-foreground-muted">
+                Additional text line
+              </div>
+              <div className="dark:text-primary-300 underline text-primary-600">
+                Additional text line
+              </div>
+            </div>
           </DialogCloseFooter>
-          <Button size="sm" variant="secondary">
-            Label
-          </Button>
-          <Button size="sm" variant="tertiary">
-            Label
-          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
