9b71994ce18e6cd0208f68023cd9c64e6a6e7612
Will Lines <110234511+williamlines@users.noreply.github.com>
2023-12-06 09:27:31 +0000

    Feat/55 website footer (#191)
    * chore: wip
    
    * feat: website footer component
    
    * chore: export types and remove duplicate prop extension
    
    * chore: improve LinkColumn variants
    
    * chore: fix width of demo items
    
    * chore: renamed title
    
    * chore: update docs
    
    * fix: docs
    
    * feat: the standard size footer is now responsive to screen size
    
    * chore: update docs
    
    * chore: renamed icon to OpenSource, made fill color inherited

---
+++ stories/WebsiteFooter/WebsiteFooter.stories.ts
@@ -0,0 +1,26 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { WebsiteFooterDemo } from './WebsiteFooter.example'
+
+const meta = {
+  title: 'Components/Website Footer',
+  component: WebsiteFooterDemo,
+  parameters: {
+    layout: 'centered'
+  }
+} satisfies Meta<typeof WebsiteFooterDemo>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const WebsiteFooterStandard: Story = {
+  args: { variant: 'standard' }
+}
+
+export const WebsiteFooterComplex: Story = {
+  args: { variant: 'complex' }
+}
+
+export const WebsiteFooterSmall: Story = {
+  args: { variant: 'small' }
+}
