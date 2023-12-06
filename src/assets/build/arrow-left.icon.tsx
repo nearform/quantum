5867f193dc6ceae37b9c020f9c6f91dc78520935
Adam Laker Illoul <85499621+Ademsk1@users.noreply.github.com>
2023-12-06 11:40:57 +0000

    Feat/190 calendar (#207)
    * feat: working calendar
    
    * feat: working calendar
    
    * feat: docs + calendar
    
    * fix: move border logic to button
    
    * fix: update docs
    
    * fix: dark mode
    
    * fix: arrow left
    
    * fix: both arrows
    
    * fix: alignment of arrows with date
    
    * fix: docs

---
+++ src/assets/build/arrow-left.icon.tsx
@@ -0,0 +1,18 @@
+const ArrowLeftIcon = (props: any) => (
+  <svg
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 14 10"
+    {...props}
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M13 5H1m0 0 4 4M1 5l4-4"
+    />
+  </svg>
+)
+export default ArrowLeftIcon
