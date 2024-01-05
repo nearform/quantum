63ad43b5b4eca5e78ebb6c837cf7fe52059697ef
Nils Stolpe <nils.stolpe@nearform.com>
2024-01-05 11:22:19 -0500

    fix: right aligned buttons on custom modal to show layout options

--- stories/Modal/ModalCustom.example.tsx
+++ stories/Modal/ModalCustom.example.tsx
@@ -10,32 +10,30 @@ import {
 } from '@/components/Modal'
 import { Button } from '@/components'
 
-export const ModalCustomDemo = () => {
-  return (
-    <Modal>
-      <Button size="sm" variant="primary" asChild>
-        <ModalTrigger>Open</ModalTrigger>
-      </Button>
-      <ModalContent>
-        <ModalHeader>
-          <ModalTitle>Title</ModalTitle>
-          <ModalClose />
-        </ModalHeader>
-        <ModalDescription asChild>
-          <div>
-            <div className="bg-blue-50 dark:bg-blue-900 dark:text-foreground-inverse h-40 text-lg font-semibold items-center flex justify-center mb-6 mx-6">
-              Replace this component with your content
-            </div>
+export const ModalCustomDemo = () => (
+  <Modal>
+    <Button size="sm" variant="primary" asChild>
+      <ModalTrigger>Open</ModalTrigger>
+    </Button>
+    <ModalContent>
+      <ModalHeader>
+        <ModalTitle>Title</ModalTitle>
+        <ModalClose />
+      </ModalHeader>
+      <ModalDescription asChild>
+        <div>
+          <div className="bg-blue-50 dark:bg-blue-900 dark:text-foreground-inverse h-40 text-lg font-semibold items-center flex justify-center mb-6 mx-6">
+            Replace this component with your content
           </div>
-        </ModalDescription>
-        <ModalFooter>
-          <div className="flex justify-start gap-2.5">
-            <Button>Label</Button>
-            <Button variant="secondary">Label</Button>
-            <Button variant="tertiary">Label</Button>
-          </div>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
-  )
-}
+        </div>
+      </ModalDescription>
+      <ModalFooter>
+        <div className="flex justify-end gap-2.5">
+          <Button variant="tertiary">Label</Button>
+          <Button variant="secondary">Label</Button>
+          <Button>Label</Button>
+        </div>
+      </ModalFooter>
+    </ModalContent>
+  </Modal>
+)
--- stories/Modal/ModalForm.example.tsx
+++ stories/Modal/ModalForm.example.tsx
@@ -11,38 +11,36 @@ import {
 } from '@/components/Modal'
 import { Button } from '@/components'
 
-export const ModalFormDemo = () => {
-  return (
-    <Modal>
-      <Button size="sm" variant="primary" asChild>
-        <ModalTrigger>Open</ModalTrigger>
-      </Button>
-      <ModalContent>
-        <ModalHeader variant="form">
-          <ModalTitle>Title</ModalTitle>
-          <ModalClose />
-        </ModalHeader>
-        <ModalDescription variant="form" asChild>
-          <div>
-            <div className="bg-blue-50 dark:bg-blue-900 dark:text-foreground-inverse h-40 text-lg font-semibold items-center flex justify-center mb-6 mx-6">
-              Replace this component with your form
-            </div>
+export const ModalFormDemo = () => (
+  <Modal>
+    <Button size="sm" variant="primary" asChild>
+      <ModalTrigger>Open</ModalTrigger>
+    </Button>
+    <ModalContent>
+      <ModalHeader variant="form">
+        <ModalTitle>Title</ModalTitle>
+        <ModalClose />
+      </ModalHeader>
+      <ModalDescription variant="form" asChild>
+        <div>
+          <div className="bg-blue-50 dark:bg-blue-900 dark:text-foreground-inverse h-40 text-lg font-semibold items-center flex justify-center mb-6 mx-6">
+            Replace this component with your form
           </div>
-        </ModalDescription>
-        <ModalFooter>
-          <Button size="lg" variant="primary" asChild>
-            <ModalCloseFooter className="w-full grid gap-2">
-              Close
-            </ModalCloseFooter>
-          </Button>
-          <div className="flex justify-center gap-4">
-            <div className="text-foreground-muted">Additional text line</div>
-            <div className="dark:text-primary-300 underline text-primary-600">
-              Additional text line
-            </div>
+        </div>
+      </ModalDescription>
+      <ModalFooter>
+        <Button size="lg" variant="primary" asChild>
+          <ModalCloseFooter className="w-full grid gap-2">
+            Close
+          </ModalCloseFooter>
+        </Button>
+        <div className="flex justify-center gap-4">
+          <div className="text-foreground-muted">Additional text line</div>
+          <div className="dark:text-primary-300 underline text-primary-600">
+            Additional text line
           </div>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
-  )
-}
+        </div>
+      </ModalFooter>
+    </ModalContent>
+  </Modal>
+)
