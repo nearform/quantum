7d0ef392380b24f8e24e94c3ac51628ca75cf84b
Andrea Forni <andrea.forni@everstream.ai>
2024-01-09 18:04:56 +0100

    fix: update Switch stories to use ControlLabel

--- stories/Switch/Switch.stories.tsx
+++ stories/Switch/Switch.stories.tsx
@@ -1,23 +1,18 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { Switch, Label } from '@/index'
+import { Switch, ControlLabel } from '@/index'
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: 'Form/Switch',
   component: Switch,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered'
   }
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 } satisfies Meta<typeof Switch>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Default: Story = {}
 
 export const Disabled: Story = {
@@ -28,20 +23,16 @@ export const Disabled: Story = {
 
 export const WithLabel: Story = {
   render: props => (
-    <div className="flex items-center space-x-1">
-      <Switch id="switchId1" {...props} />
-      <Label htmlFor="switchId1">Label</Label>
-    </div>
+    <ControlLabel htmlFor="withLabelId" label="Label">
+      <Switch id="withLabelId" {...props} />
+    </ControlLabel>
   )
 }
 
 export const WithLabelAndHint: Story = {
   render: props => (
-    <div className="flex items-center space-x-1">
-      <Switch id="switchId1" {...props} />
-      <Label htmlFor="switchId1" hintText="This is a hint">
-        Label
-      </Label>
-    </div>
+    <ControlLabel htmlFor="withHintId" label="Label" hintText="This is a hint">
+      <Switch id="withHintId" {...props} />
+    </ControlLabel>
   )
 }
