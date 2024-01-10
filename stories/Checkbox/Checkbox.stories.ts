5bbb6b0c5df2e21436ee818e27906666e18bbb44
Andrea Forni <andrea.forni@everstream.ai>
2024-01-10 12:27:43 +0100

    refactor: rename stories and remove unuseful comments

--- stories/Checkbox/Checkbox.stories.ts
+++ stories/Checkbox/Checkbox.stories.ts
@@ -2,15 +2,12 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import { CheckboxDemo } from './CheckBox.example'
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: 'Form/Checkbox',
   component: CheckboxDemo,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered'
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   argTypes: {
     checked: {
       options: [true, false, 'indeterminate'],
@@ -21,30 +18,28 @@ const meta = {
       description: 'Alter the className to change the style'
     }
   }
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 } satisfies Meta<typeof CheckboxDemo>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const CheckBoxUnchecked: Story = {
+export const Unchecked: Story = {
   args: {
     checked: false
   }
 }
-export const CheckBoxChecked: Story = {
+export const Checked: Story = {
   args: {
     checked: true
   }
 }
-export const CheckBoxIndeterminate: Story = {
+export const Indeterminate: Story = {
   args: {
     checked: 'indeterminate'
   }
 }
 
-export const CheckboxColourChange: Story = {
+export const ColourChange: Story = {
   args: {
     checked: true,
     className:
