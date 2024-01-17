e1309c1f498c45a85afd828dc52206e7cdb9e38a
Andrea Forni <andrea.forni@everstream.ai>
2024-01-17 09:50:30 +0100

    fix: update table docs and simplify examples

--- stories/Table/Table.stories.tsx
+++ stories/Table/Table.stories.tsx
@@ -7,12 +7,8 @@ import {
   TableCell,
   TableHead,
   TableHeader,
-  TableRow,
-  Label,
-  Checkbox
+  TableRow
 } from '@/components'
-
-import { BsPersonCircle } from '@/assets'
 
 const meta = {
   title: 'Components/Table',
@@ -25,55 +21,41 @@ const meta = {
         <TableCaption>A list of your recent invoices.</TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead>Header Cell</TableHead>
-            <TableHead>
-              <div className="flex items-center">
-                <BsPersonCircle className="mr-2" /> User
-              </div>
-            </TableHead>
+            <TableHead>User</TableHead>
             <TableHead>Method</TableHead>
             <TableHead>Amount</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody variant={props.variant}>
           <TableRow>
-            <TableCell>Data Cell</TableCell>
             <TableCell>Freeman</TableCell>
             <TableCell>Credit Card</TableCell>
             <TableCell>$250.00</TableCell>
           </TableRow>
           <TableRow>
-            <TableCell className="space-x-2">
-              <Checkbox />
-              <Label>Insert a checkbox</Label>
-            </TableCell>
             <TableCell>Vance</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell>$250.00</TableCell>
+            <TableCell>Cash</TableCell>
+            <TableCell>$50.00</TableCell>
           </TableRow>
           <TableRow>
-            <TableCell>Data Cell</TableCell>
             <TableCell>Kleiner</TableCell>
             <TableCell>Credit Card</TableCell>
-            <TableCell>$250.00</TableCell>
+            <TableCell>$1400.00</TableCell>
           </TableRow>
           <TableRow>
-            <TableCell>Data Cell</TableCell>
             <TableCell>Mossman</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell>$250.00</TableCell>
+            <TableCell>Cash</TableCell>
+            <TableCell>$30.00</TableCell>
           </TableRow>
           <TableRow>
-            <TableCell>Data Cell</TableCell>
             <TableCell>Breen</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell>$250.00</TableCell>
+            <TableCell>Cash</TableCell>
+            <TableCell>$75.50</TableCell>
           </TableRow>
           <TableRow>
-            <TableCell>Data Cell</TableCell>
             <TableCell>Calhoun</TableCell>
             <TableCell>Credit Card</TableCell>
-            <TableCell>$250.00</TableCell>
+            <TableCell>$400.00</TableCell>
           </TableRow>
         </TableBody>
       </Table>
