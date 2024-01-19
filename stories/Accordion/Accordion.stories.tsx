8b63d74c9ada6b1af1cc5f50d05a6dd284408076
Andrea Forni <andrea.forni@everstream.ai>
2024-01-19 15:34:20 +0100

    fix: move Accordion example inside the story

--- stories/Accordion/Accordion.stories.tsx
+++ stories/Accordion/Accordion.stories.tsx
@@ -1,24 +1,68 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { AccordionDemo } from './Accordion.example'
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent
+} from '@/components'
 
 const meta = {
   title: 'Components/Accordion',
-  component: AccordionDemo,
+  component: Accordion,
   parameters: {
     layout: 'centered'
   },
   argTypes: {
-    className: {
-      controle: 'text',
-      description: 'Alter the className to change the style'
+    type: {
+      options: ['single', 'multiple'],
+      control: 'radio'
     }
-  }
-} satisfies Meta<typeof AccordionDemo>
+  },
+  render: props => (
+    <Accordion {...props}>
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Item 1</AccordionTrigger>
+        <AccordionContent>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          facilisis ac ligula ut luctus. Nulla sed tortor tempor, auctor mi at,
+          mollis nulla.
+        </AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem value="item-2" className="">
+        <AccordionTrigger>Item 2</AccordionTrigger>
+        <AccordionContent>
+          <div className="text-sm px-6 text-foreground-muted bg-blue-50 dark:bg-blue-900 dark:text-foreground-inverse h-40 font-semibold items-center flex justify-center">
+            Replace this component with your content
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem value="item-3">
+        <AccordionTrigger>Item 3</AccordionTrigger>
+        <AccordionContent>
+          <div className="bg-blue-50 dark:bg-blue-900 dark:text-foreground-inverse h-40 font-semibold items-center flex justify-center text-sm px-6 text-foreground-muted">
+            Replace this component with your content
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  )
+} satisfies Meta<typeof Accordion>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {
-  args: {}
+export const Single: Story = {
+  args: {
+    type: 'single',
+    collapsible: true
+  }
+}
+
+export const Multiple: Story = {
+  args: {
+    type: 'multiple'
+  }
 }
